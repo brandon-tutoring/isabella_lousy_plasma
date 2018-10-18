9a324dfb91174526ffce466fad5a2cb4ef2c3e52
Dora83 <43157814+Dora83@users.noreply.github.com>
2018-10-18 09:05:57 -0400

    Update lousy_plasma0.py

--- lousy_plasma0.py
+++ lousy_plasma0.py
@@ -1,36 +1,39 @@
+import string
 import random
 
 def remove_punctuation(var):
     no_punct = ""
-    punctuation =  '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    for item in var: 
-        if item not in punctuation:
-            no_punct += item + " " 
+    for item in var:
+        if item not in string.punctuation:
+            no_punct += item
     return no_punct
+
+
+t = open('thesaurus', 'r')
+dict1 = { "happy": ["GLAD", "BLISSFUL", "ECSTATIC"], "sad"  : ["BLEAK", "BLUE", "DEPRESSED"], "baby": ["FOOL","INFANT"], "bad": ["BADNESS","EVIL","FETOR","UNCLEANESS","WRONG"], "blood":["FOP","KILLING","FLUIDITY"]}
+for line in t:
+    split_words = line.split(",")
+    key = split_words.pop([0])
+    syn = {split_words[0]:split_words - split_word[0]}
+    print(syn)
+    break
+
 
 def lousy_plasma():
 
-    thesaurus = { "happy": ["GLAD", "BLISSFUL", "ECSTATIC"], "sad"  : ["BLEAK", "BLUE", "DEPRESSED"], "baby": ["FOOL","INFANT"], "bad": ["BADNESS","EVIL","FETOR","UNCLEANESS","WRONG"], "blood":["FOP","KILLING","FLUIDITY"]}
-
+    empty_string = ""          
     input1 = input("what is your sentence?").strip()
-
-    words = input1.split(" ")
-    remove_punctuation(words)    
-
-    list1 = []
-
-    for item in words:
-        if item not in thesaurus.keys():
-            list1.append(item)
+    input1 = input1.split()
+    input1 = remove_punctuation(input1).split()
+    for item in input1:
+        if item in dict1.keys():
+            empty_string += random.choice(dict1[item]) + " "
         else:
-            values = thesaurus.get(item)
-            list1.append(random.choice(values))
-    list1.join()
-
+            empty_string += item + " "
+    print(empty_string)
     
 
-    print(list1)
-   
 
  
 lousy_plasma()
+
