d297b1727d34697dcb04e2859539ba204a611a02
Isabella Schell <Isabella@Macintosh-4.local>
2018-10-25 22:14:48 -0400

    fixed file path

--- lousy_plasma0.py
+++ lousy_plasma0.py
@@ -13,18 +13,16 @@ t = open('thesaurus', 'r')
 dict1 = {}
 for line in t:
     split_words = line.split(",")
-    key = split_words.pop([0])
-    dict1.update({key:split_words)
+    key = split_words.pop(0)
+    dict1.update({key:split_words})
 
 
 def lousy_plasma():
 
     empty_string = ""          
     input1 = input("what is your sentence?").strip()
-    print(input1)
     input1 = remove_punctuation(input1).split()
-    print(input1)
-    return
+
     for item in input1:
         if item in dict1.keys():
             empty_string += random.choice(dict1[item]) + " "
